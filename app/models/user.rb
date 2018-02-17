323c907d4ce2903bd0770b2a8a1d621f3588aa78
RN <attinyes@gmail.com>
2018-02-18 02:17:07 +0900

    [update] Userモデルにname,emailカラムとそのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal:true
 class User < ApplicationRecord
   has_secure_password
+  validates :name, presence: true, length: { maximum: 255 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
