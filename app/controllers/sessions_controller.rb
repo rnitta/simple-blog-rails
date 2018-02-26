85ef39da65d7bfb53fc0746446990e34e3c54806
RN <attinyes@gmail.com>
2018-02-26 17:36:34 +0900

    [add] ログイン機能になるnewとcreateアクションを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
   def new
+  end
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user&.authenticate(params[:session][:password])
+      redirect_to user
+    else
+      @login_error = true
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   resources :users, only: [:new, :create, :show]
+  get '/login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
 end
