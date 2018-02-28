9a08bd4b76c51ce0dfc8c5126b355b687590d5b5
RN <attinyes@gmail.com>
2018-02-28 14:50:35 +0900

    [add] バリデーションとリレーションを追加

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal:true
+
 class Blog < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :description, presence: true, length: { maximum: 255 }
+  validates :url, presence: true,
+                  length: { maximum: 255 },
+                  format: { with: /\w/ },
+                  uniqueness: true
 end
