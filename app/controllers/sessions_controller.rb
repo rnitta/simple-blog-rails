df5e264dad05dbf6337f0efeed8107b49d546353
RN <attinyes@gmail.com>
2018-03-01 07:10:43 +0900

    [fix] newとcreateアクションを修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,11 @@
 
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user&.authenticate(params[:session][:password])
+    user = User.find_by(email: user_params[:email])
+    if user&.authenticate(user_params[:password])
       log_in(user)
       redirect_to(user)
     else
@@ -16,5 +17,11 @@ class SessionsController < ApplicationController
   def destroy
     log_out
     redirect_to('/')
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
