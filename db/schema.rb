778fdb0a3b1c4359520f02d3b90a49361fe1841d
RN <attinyes@gmail.com>
2018-02-28 15:19:29 +0900

    [update] '$ rails db:migrate' 実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180217171002) do
+ActiveRecord::Schema.define(version: 20180228054214) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "url"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
