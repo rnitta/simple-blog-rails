59681b9e4139ff1a58e3316fc4e699eb5be02b33
RN <attinyes@gmail.com>
2018-03-01 17:16:02 +0900

    [update] バリデーションを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,9 @@
 class Article < ApplicationRecord
   belongs_to :blog
   belongs_to :category
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :url, presence: true,
+                  length: { maximum: 255 },
+                  format: { with: /\w/ },
+                  uniqueness: { scope: :blog }
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,5 @@
 class Category < ApplicationRecord
   belongs_to :blog
   has_many :articles
+  validates :name, presence: true, length: { maximum: 255 }
 end
