1150f2d5f5f2a17b8da23ca37aa6d6f0fcfac90b
RN <attinyes@gmail.com>
2018-03-02 09:58:46 +0900

    [fix] urlカラムをnameカラムに名前変更

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,8 +4,8 @@ class Blog < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: { maximum: 255 }
   validates :description, presence: true, length: { maximum: 255 }
-  validates :url, presence: true,
-                  length: { maximum: 255 },
-                  format: { with: /\w/ },
-                  uniqueness: true
+  validates :name, presence: true,
+                   length: { maximum: 255 },
+                   format: { with: /\w/ },
+                   uniqueness: true
 end
