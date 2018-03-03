971282e094e2816f9756125e5a81247147e9610c
RN <attinyes@gmail.com>
2018-03-03 23:18:23 +0900

    [add] '$ rails g controller Blogs show new edit --no-assets' 実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
+  get 'blogs/show'
+
+  get 'blogs/new'
+
+  get 'blogs/edit'
+
   resources :users, only: [:new, :create, :show]
   get '/login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
