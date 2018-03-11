c2426d406753a163a866b77714b9f7540854f890
RN <attinyes@gmail.com>
2018-03-11 13:14:30 +0900

    [add] ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  get 'blogs/show'
-
-  get 'blogs/new'
-
-  get 'blogs/edit'
-
   resources :users, only: [:new, :create, :show]
+  resources :blogs, except: [:index]
   get '/login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
