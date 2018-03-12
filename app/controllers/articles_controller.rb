94884d95398fc531d06c48e946a8d0172b16cadd
RN <attinyes@gmail.com>
2018-03-13 02:20:58 +0900

    [add] indexアクションとビューを作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal:true
 
 class ArticlesController < ApplicationController
+  include BlogsHelper
+  before_action :set_blog, only: [:index]
+  before_action :set_article, only: []
+
   def index
+    if authorized?(@blog)
+      @articles = Blog.friendly.find(params[:blog_name]).articles
+    else
+      redirect_to(blog_path(@blog.name))
+    end
   end
 
   def show
@@ -23,6 +32,10 @@ class ArticlesController < ApplicationController
   end
 
   private
+
+  def set_blog
+    @blog = Blog.friendly.find(params[:blog_name])
+  end
 
   def set_article
     @article = Blog.friendly.find(params[:blog_name]).articles.friendly.find(params[:name])
