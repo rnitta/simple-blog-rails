08f60c1032e97171673fde75496383ff6448f611
RN <attinyes@gmail.com>
2018-03-13 02:00:31 +0900

    [add] 各アクションの雛形を追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,30 @@
+# frozen_string_literal:true
+
 class ArticlesController < ApplicationController
+  def index
+  end
+
   def show
   end
 
   def new
   end
 
+  def create
+  end
+
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_article
+    @article = Blog.friendly.find(params[:blog_name]).articles.friendly.find(params[:name])
   end
 end
