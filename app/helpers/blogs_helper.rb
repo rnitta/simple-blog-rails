43625c2193d0f7161cde3179a059d578c8afddaf
RN <attinyes@gmail.com>
2018-03-13 16:55:29 +0900

    [fix] 真偽値を返すだけに修正

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,10 +2,6 @@
 
 module BlogsHelper
   def authorized?(blog)
-    if current_user.nil?
-      redirect_to(login_path)
-    else
-      blog.user_id == current_user.id
-    end
+    blog.user_id == current_user&.id
   end
 end
