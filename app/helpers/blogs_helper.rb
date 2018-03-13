8f8693e8a39b22c7f9ae70cf211550d871be3f0b
RN <attinyes@gmail.com>
2018-03-13 16:41:43 +0900

    [fix] 非ログイン時にauthorized?が呼ばれるとエラーがでるのを修正

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,6 +2,10 @@
 
 module BlogsHelper
   def authorized?(blog)
-    blog.user_id == current_user.id
+    if current_user.nil?
+      redirect_to(login_path)
+    else
+      blog.user_id == current_user.id
+    end
   end
 end
