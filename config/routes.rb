f2dabbd3a385081d2334ce517bf0c6ac6be4b20b
RN <attinyes@gmail.com>
2018-03-15 15:29:37 +0900

    [update] ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  get 'categories/index'
-
-  get 'categories/show'
-
-  get 'categories/new'
-
-  get 'categories/edit'
-
   resources :users, only: [:new, :create, :show]
   resources :blogs, except: [:index, :create], param: :name do
     resources :articles, param: :name
+    resources :categories
   end
   get '/login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
