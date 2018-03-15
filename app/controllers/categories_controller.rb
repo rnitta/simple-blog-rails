9dd201d6822169d55020956886b6c4452270b8bc
RN <attinyes@gmail.com>
2018-03-15 16:02:07 +0900

    [add] 各アクションの雛形を追加

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal:true
+
 class CategoriesController < ApplicationController
+  include BlogsHelper
+  before_action :require_authority, except: [:show, :create]
+  before_action :set_blog, only: [:index, :show, :new, :destroy]
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
   def index
   end
 
@@ -9,5 +15,30 @@ class CategoriesController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_blog
+    @blog = Blog.friendly.find(params[:blog_name])
+  end
+
+  def set_category
+    @category = Blog.friendly.find(params[:blog_name]).categories.find(params[:id])
+  end
+
+  def require_authority
+    set_blog
+    redirect_to(blog_path(@blog.name)) unless authorized?(@blog)
+  end
+
+  def category_params
+    params.require(:article).permit(:name)
   end
 end
